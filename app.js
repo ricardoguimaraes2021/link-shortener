1492a0d1bc33f1ef30f364a6f0c1abf3f8614b00
Ricardo Guimarães <ricardoguimaraes.info@gmail.com>
2024-09-27 20:36:35 +0100

    Getting online

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const app = express();
 const urlRoutes = require('./routes/urlRoutes'); // Import URL routes
 
-// Set up the port (using 5000 or a port from environment variables)
+// Set up the port (using 5001 or a port from environment variables)
 const PORT = process.env.PORT || 5001;
 
 // Middleware to parse JSON and URL-encoded data
@@ -16,10 +16,13 @@ app.use(express.urlencoded({ extended: true }));
 // Serve static files (for frontend)
 app.use(express.static('public'));
 
-// Connect to MongoDB locally using Mongoose
+// Check if MongoDB URI is loaded correctly from the .env file
+console.log('MongoDB URI:', process.env.MONGO_URI);
+
+// Connect to MongoDB Atlas using Mongoose
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB Connected Locally'))
-  .catch(err => console.log('Error connecting to MongoDB:', err));
+  .then(() => console.log('MongoDB Atlas Connected'))
+  .catch(err => console.log('Error connecting to MongoDB Atlas:', err));
 
 // Use the URL routes
 app.use('/', urlRoutes);
